d361aa1c7ed6255f21e5bdf2f3f2d37905c5e38b
lka <admin@lka-mac.cust.communityfibre.co.uk>
2024-06-07 16:20:06 +0100

    clean page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
 "use client";
 
-import styled from "styled-components";
-// import { Button } from "../components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "../components/ui/card";
-
 import dynamic from "next/dynamic";
-
-const Container = styled.div`
-  /* background: #fff; */
-`;
 
 const Feed = dynamic(() => import("../app/components/molecules/Feed"), {
   ssr: false,
@@ -24,59 +9,7 @@ const Feed = dynamic(() => import("../app/components/molecules/Feed"), {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2 drop-shadow-md">
-      <Container>
-        <Feed />
-        {/* <div className="grid  grid-cols-4 gap-4">
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        </div> */}
-      </Container>
+      <Feed />
     </main>
   );
 }
