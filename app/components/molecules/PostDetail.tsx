6476c73e57e1b76532030fc4a31c26deff0fd3b7
lka <admin@lka-mac.cust.communityfibre.co.uk>
2024-06-07 16:18:20 +0100

    improve post details ui

--- app/components/molecules/PostDetail.tsx
+++ app/components/molecules/PostDetail.tsx
@@ -1,5 +1,6 @@
 import { Post, User } from "../../interfaces";
 import Image from "next/image";
+import Link from "next/link";
 
 interface PostDetailProps {
   post: Post;
@@ -8,25 +9,33 @@ interface PostDetailProps {
 
 const PostDetail: React.FC<PostDetailProps> = ({ post, author }) => {
   return (
-    <div className="p-6">
-      Details
+    <div className="p-6 text-center">
+      <div className="flex justify-start mb-4">
+        <Link legacyBehavior href="/">
+          <a className="text-xl font-bold hover:underline">&lt; Home</a>
+        </Link>
+      </div>
       {author && (
-        <div className="flex items-center mb-4">
-          <Image
-            src={`https://i.pravatar.cc/150?img=${author.id}`}
-            alt={`Avatar of ${author.name}`}
-            width={150}
-            height={150}
-            className="w-24 h-24 rounded-full mr-4"
-            loading="lazy"
-          />
+        <div className="mb-8">
+          <div className="flex justify-center mb-4">
+            <Image
+              src={`https://i.pravatar.cc/200?img=${author.id}`}
+              alt={`Avatar of ${author.name}`}
+              width={200}
+              height={200}
+              className="rounded-full"
+              loading="lazy"
+            />
+          </div>
           <h2 className="text-2xl font-bold">{author.name}</h2>
         </div>
       )}
-      <div className="mt-4">
-        <h1 className="text-3xl font-semibold mb-4">{post.title}</h1>
-        <p className="text-gray-700">{post.body}</p>
-      </div>
+      {post && (
+        <div className="p-4 mt-8 border rounded-lg bg-gray-100 transition duration-200 shadow-md">
+          <h1 className="text-3xl font-semibold mb-4">{post.title}</h1>
+          <p className="text-gray-700">{post.body}</p>
+        </div>
+      )}
     </div>
   );
 };
